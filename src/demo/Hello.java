9555680caaf31a3c4483967219ed66ed0245f9e6
cch <1136856486@qq.com>
2018-08-23 01:12:20 +0800

    11

--- src/demo/Hello.java
+++ src/demo/Hello.java
@@ -1,9 +1,26 @@
 package demo;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Hello {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		File file = new File("D:\\test\\123.txt");//定义一个file对象，用来初始化FileReader
+        FileReader reader = new FileReader(file);//定义一个fileReader对象，用来初始化BufferedReader
+        BufferedReader bReader = new BufferedReader(reader);//new一个BufferedReader对象，将文件内容读取到缓存
+        StringBuilder sb = new StringBuilder();//定义一个字符串缓存，将字符串存放缓存中
+        String s = "";
+        while ((s =bReader.readLine()) != null) {//逐行读取文件内容，不读取换行符和末尾的空格
+            sb.append(s + "\n");//将读取的字符串添加换行符后累加存放在缓存中
+           // System.out.println(s);
+        }
+        bReader.close();
+        String str = sb.toString();
+        System.out.println(str );
 
 	}
 
